7b7515f53a93682cdf594da18ceb20c341b8c4ed
Sumit Mahour <sumitmahour24@gmail.com>
2022-10-02 21:07:11 +0530

    OOP added

---
+++ 19_OOP/01_class.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+class Car{
+
+    // public modifier
+    public:
+    string carName;
+    int topSpeed;
+
+    // private modifier 
+    private:
+    int price=10000;
+    public:
+    void checkPrice(){
+        cout<<price<<" ";
+    }
+
+
+};
+
+
+
+int main(){
+
+    Car car;
+    car.carName="Honda";
+    car.topSpeed=120;
+    cout<<"The car name is:"<<car.carName<<endl;
+    cout<<"The car's top speed is:"<<car.topSpeed<<endl;
+    car.checkPrice();
+
+}
