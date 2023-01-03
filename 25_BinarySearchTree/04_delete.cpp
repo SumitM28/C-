e9ee583f1350816f3d3be7a70328dd531868d9f4
Sumit Mahour <sumitmahour24@gmail.com>
2023-01-03 15:50:58 +0530

    added

--- 25_BinarySearchTree/04_delete.cpp
+++ 25_BinarySearchTree/04_delete.cpp
@@ -83,34 +83,71 @@ void levelOrderTraversal(Node *root)
         }
     }
 }
-void *deleteNode(Node* &root, int data)
-{
-    if (root == NULL || root->data == data)
+
+Node* findMini(Node* root){
+    while (root->left!=NULL)
     {
-        root = NULL;
-        return 0;
-    }
-    // if node present in left subtree
-    if (root->left->data == data)
-    {
-        root->left = NULL;
-    }
-    // if node present in right subtree
-    if (root->right->data == data)
-    {
-        root->left = NULL;
+        root=root->left;
     }
 
-    // check in left subtree
-    if (root->data > data)
+    return root;
+    
+}
+Node *deleteFromBST(Node *root, int data)
+{
+
+    if (root == NULL)
     {
-        deleteNode(root->left, data);
+        return root;
     }
-    // checck in right subtree
-    if (root->data < data)
+
+    if (root->data == data)
     {
-        deleteNode(root->right, data);
+
+        // 0 child
+        if (root->left == NULL && root->right == NULL)
+        {
+            delete root;
+            return NULL;
+        }
+
+        // 1 child
+        // only left child exist
+        if (root->left !=NULL && root->right == NULL)
+        {
+            Node* temp=root->left;
+            delete root;
+            return temp;
+        }
+        // only right child exist
+        if (root->right!=NULL && root->left == NULL)
+        {
+            Node* temp=root->right;
+            delete root;
+            return temp;
+        }
+
+        // 2 child
+        if (root->left !=NULL && root->right !=NULL)
+        {
+            int mini=findMini(root->right) -> data;
+            root->data=mini;
+            root->right= deleteFromBST(root->right,mini);
+            return root; 
+        }
     }
+    else if (root->data > data)
+    {
+        // check in left part
+        deleteFromBST(root->left, data);
+    }
+    else
+    {
+        // check in right part
+        deleteFromBST(root->right, data);
+    }
+
+    return root;
 }
 
 int main()
@@ -119,7 +156,8 @@ int main()
     Node *root = NULL;
     cout << "Enter the input:" << endl;
     takeInput(root);
-    // levelOrderTraversal(root);
-    deleteNode(root, 5);
     levelOrderTraversal(root);
+    deleteFromBST(root, 5);
+    levelOrderTraversal(root);
+
 }
